e39bb08c50094cbaa5e814717f59d275e5169455
kelinzzc <zzc030817@163.com>
2025-04-15 11:11:39 +0800

    4

--- Assets/ZicgenZhang/jiaoben/juese.cs
+++ Assets/ZicgenZhang/jiaoben/juese.cs
@@ -16,16 +16,18 @@ public class juese : MonoBehaviour
             _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
         }
         _controller = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
     }
 
-    public float speed = 6.0f;
+    Animator _animator;
     float _targetRot = 0.6f;
     public float RotationSmoothTime = 0.1f;
     float _rotationVelocity;
+    public float moveSpeed = 100000f;
     // Update is called once per frame
     void Update()
     {
-        Vector3 velocity = new Vector3(0,-1,0);
+
         if (_move != Vector2.zero)
         {
             Vector3 inputDir = new Vector3(_move.x,0.0f,_move.y).normalized;
@@ -33,10 +35,19 @@ public class juese : MonoBehaviour
             float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y,_targetRot, ref _rotationVelocity, RotationSmoothTime);
 
             transform.rotation = Quaternion.Euler(0.0f,rotation,0.0f);
-            Vector3 targetDir = Quaternion.Euler(0.0f,_targetRot,0.0f) * Vector3.forward;
-            velocity += targetDir.normalized * (speed * Time.deltaTime);
         }
-        _controller.Move(velocity);
+        var axisY = _animator.GetFloat("AxisY");
+        axisY = Mathf.MoveTowards(axisY,_move.magnitude,5f * Time.deltaTime);
+        _animator.SetFloat("AxisY",axisY);
+
+         if (_move != Vector2.zero) 
+    {
+        // 计算移动方向（基于角色旋转后的正前方）
+        Vector3 moveDirection = Quaternion.Euler(0.0f, _targetRot, 0.0f) * Vector3.forward;
+        // 应用移动速度并执行移动
+        _controller.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
+    }
+
     }
     Vector2 _move;
     void OnMove(InputValue value)
